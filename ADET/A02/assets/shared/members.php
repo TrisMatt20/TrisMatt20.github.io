2a96217911b016fb240586e94f9c75aeaf788e0d
TrisMatt20 <tmtmatencio@gmail.com>
2025-04-12 16:10:35 +0800

    added responsive members page

---
+++ ADET/A02/assets/shared/members.php
@@ -0,0 +1,108 @@
+<div class="container">
+    <div class="row mt-3 pt-3 justify-content-center">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_aiah.jpg" alt="aiah" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Aiah</h5>
+                        <p class="mb-0">Main Rapper / Visual</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_colet.jpg" alt="colet" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Colet</h5>
+                        <p class="mb-0">Main Vocal / Lead Dancer </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_maloi.jpg" alt="maloi" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Maloi</h5>
+                        <p class="mb-0">Main Vocal</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_gwen.jpg" alt="gwen" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Gwen</h5>
+                        <p class="mb-0">Lead Vocal / Lead Rapper</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_stacey.jpg" alt="stacey" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Stacey</h5>
+                        <p class="mb-0">Main Rapper / Sub Vocalist</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_mikha.jpg" alt="mikha" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Mikha</h5>
+                        <p class="mb-0">Main Rapper / Lead Dancer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_jhoanna.jpg" alt="jhoanna" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Jhoanna</h5>
+                        <p class="mb-0">Leader / Lead Vocal</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="assets/img/members/bt_sheena.jpg" alt="sheena" class="img-fluid">
+                    </div>
+                    <div class="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                        <h5 class="mb-1">BINI Sheena</h5>
+                        <p class="mb-0">Main Dancer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
